59caba3845c5d2a0fc0a891e65fadd6dcb222740
Pauzer <63166329+MrPauzer@users.noreply.github.com>
2021-04-06 11:15:54 +0200

    Add files via upload

---
+++ logi/config.lua
@@ -0,0 +1,36 @@
+Config = {}
+
+Config.AllLogs = true									-- Włącz / wyłącz wszystkie logi
+Config.username = "Nazwa Bota" 							-- Nazwa bota
+Config.avatar = ""				                    	-- Avatar bota w rozmiarze 30x30 (Musi być link do zdjęcia)
+Config.communtiyName = "Nazwa Community"			 	-- Community nazwa
+Config.communtiyLogo = ""								-- Ikona community w rozmiarze 30x30 (Musi być link do zdjęcia)
+
+
+
+Config.weaponLog = true  			-- ustaw na false, jeżeli nie chcesz logów ze strzelania
+Config.playerID = true				-- ustaw na false, jeżeli nie chcesz logów z id gracza
+Config.steamID = true				-- ustaw na false, jeżeli nie chcesz logów z steam id gracza
+Config.steamURL = true				-- ustaw na false, jeżeli nie chcesz logów z steam url gracza
+Config.discordID = true				-- ustaw na false, jeżeli nie chcesz logów z discord id gracza
+Config.license = true				-- ustaw na false, jeżeli nie chcesz logów z licencji rockstar gracza
+Config.IP = true					-- ustaw na false, jeżeli nie chcesz logów z IP gracza
+ 
+Config.joinColor = "#3AF241" 	 	-- Kolor podczas dołączania gracza na serwer
+Config.leaveColor = "#F23A3A"		-- Kolor podczas wychodzenia gracza z serwera
+Config.chatColor = "#A1A1A1"		-- Kolor czatu
+Config.shootingColor = "#2E66F2"	-- Kolor strzelania z broni
+Config.deathColor = "#000000"		-- Koloros śmierci gracza
+Config.resourceColor = "#EBEE3F"	-- Kolor Resource
+
+
+
+Config.webhooks = {
+	all = "",			-- Link do WebHooka
+	chat = "",			-- Link do WebHooka
+	joins = "",			-- Link do WebHooka
+	leaving = "",		-- Link do WebHooka
+	deaths = "",		-- Link do WebHooka
+	shooting = "",		-- Link do WebHooka
+	resources = "",		-- Link do WebHooka
+}
---
+++ logi/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+server_script 'config.lua'
+
+server_script 'server/server.lua'
+
+client_script 'client/client.lua'
+
+
